f8423aa1efef1585670dbee4a0308e803bef63e0
Wellington2708 <wellingtondevelopercontact@gmail.com>
2023-03-30 21:40:51 +0100

    add button with submit

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import './formulario.css'
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
 
@@ -12,16 +13,24 @@ const Formulario = () => {
         'Ux e Design',
         'Mobile',
         'Inovação e Gestão'
-        ]
+        ];
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    };    
 
     return (
         <section className='formulario'> 
-            <form>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa label="Time" itens={times}/>
-                
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
                 
                 
             </form>
